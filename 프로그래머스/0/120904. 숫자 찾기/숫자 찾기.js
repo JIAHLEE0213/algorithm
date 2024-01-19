c8401f8bdab86ee75e4423f94cf250421538a1dc
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-01-19 12:22:34 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -0,0 +1,7 @@
+function solution(num, k) {
+    // const arr = [...String(num)]
+    // num에서 k의 수와 같은게 잇다면 자리수 return/ 없으면 -1
+    const string = String(num)
+    const filter =  string.includes(k)
+    return filter ? string.indexOf(k)+1 : -1
+}
