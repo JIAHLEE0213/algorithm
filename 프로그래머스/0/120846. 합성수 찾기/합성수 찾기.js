7ff453f0cd3e14972b439d89fb52ab61668efc2b
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-01-23 14:36:15 +0900

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -4,13 +4,9 @@ function solution(n) {
     for (let i = 1; i <= n; i++) {
             let count = 0
         for(let j = 1; j <= i; j++){
-            if(i % j === 0) {
-                count++
-            }
+            if(i % j === 0) count++
         }
-        if(count >= 3) {
-             result++
-        }
+        if(count >= 3) result++
     }
     return result;
 }
