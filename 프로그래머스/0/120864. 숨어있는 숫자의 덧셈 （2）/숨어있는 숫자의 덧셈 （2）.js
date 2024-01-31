f6b6e695b68f2444c09de7d6110e88f107e1da10
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-01-31 14:21:23 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,19 @@
+function solution(my_string) {
+    let count = 0;
+    let num = 0;
+
+    for (let i = 0; i < my_string.length; i++) {
+        // 현재 i가 숫자면 누적, 아니면 더하기
+        if (!isNaN(Number(my_string[i]))) {
+            num = num * 10 + Number(my_string[i]);
+        } else {
+            count += num;
+            num = 0;
+        }
+    }
+
+    // 마지막으로 남아있는 숫자를 합산
+    count += num;
+
+    return count;
+}
