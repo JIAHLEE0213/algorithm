11b66b20de66ce8e7c8753d8a8cc8a580120407f
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-01-31 14:25:13 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,6 +1,6 @@
 function solution(my_string) {
-    let count = 0;
-    let num = 0;
+    let count = 0; // 숫자인 수 더해주기
+    let num = 0; // 연속인 숫자 쌓기 위해 위함
 
     for (let i = 0; i < my_string.length; i++) {
         // 현재 i가 숫자면 누적, 아니면 더하기
