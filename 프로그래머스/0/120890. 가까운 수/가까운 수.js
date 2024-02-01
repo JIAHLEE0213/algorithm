3bbd4d7d3e16de42aaa176f458943812b1b5edce
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-02-01 11:16:45 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,11 +1,6 @@
 function solution(array, n) {
     // array의 원소의 값과 n에서 차이가 적은걸 return
     const arr = array.sort();
-    return arr.reduce((acc, cur) => {
-        if(Math.abs(acc - n) > Math.abs(cur - n)) {
-            return cur;
-        } else {
-            return acc; 
-        }
-    })
+    return arr.reduce((acc, cur) => 
+        Math.abs(acc - n) > Math.abs(cur - n) ? cur : acc)
 }
