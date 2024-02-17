2f9336c113781f4070bf6c8f7ad4a2b36555dc2e
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-02-17 23:20:23 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,25 @@
+function solution(numbers) {
+    let obj = {
+        zero:0,
+        one:1,
+        two:2,
+        three:3,
+        four:4,
+        five:5,
+        six:6,
+        seven:7,
+        eight:8,
+        nine:9
+    }
+    let result = [];
+    let str = [];
+    for(let i = 0; i < numbers.length; i++){
+        str.push(numbers[i])
+        
+        if(obj[str.join('')] !== undefined) {
+            result.push(obj[str.join('')])
+            str = []
+        } 
+    }
+    return Number(result.join(''));
+}
