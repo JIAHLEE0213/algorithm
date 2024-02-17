827f7e5325061a26a8dfd49e746e5d24c33f0643
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-02-17 22:48:07 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,5 +1,6 @@
 function solution(my_string) {
     // 중복된 문자나 공백 제거
     return [...new Set(my_string)].join('')
+    // const arr = [...my_string]
     // return arr.filter((el,index) => arr.indexOf(el) === index).join('')
 }
