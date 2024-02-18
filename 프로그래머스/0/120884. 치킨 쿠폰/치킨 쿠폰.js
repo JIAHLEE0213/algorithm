c279c1fecf894c77f4847b198f58edd8671b7a39
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-02-18 14:17:47 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -3,6 +3,7 @@ function solution(chicken) {
     
     while(chicken >= 10) {
         result += Math.floor(chicken/10);
+        // 치킨을 시킨만큼 쿠폰을 더 받는 것과 나머지 쿠폰갯수를 더해주기
         chicken = Math.floor(chicken/10) + chicken % 10;
     }
     
