41c394d77bd7bf7126155538a330c5e1351d6fc9
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-12-18 17:33:25 +0900

    [level 0] Title: 5명씩, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -0,0 +1,9 @@
+function solution(names) {
+    let arr = [];
+    for(let i = 0; i < names.length; i++) {
+        if(i % 5 === 0) {
+            arr.push(names[i])
+        } 
+    }
+    return arr;
+}
