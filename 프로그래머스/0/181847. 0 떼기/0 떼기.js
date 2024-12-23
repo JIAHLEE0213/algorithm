00faacf300ba7ee0f217f88032c86fb7495db908
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-12-23 15:59:43 +0900

    [level 0] Title: 0 떼기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,8 +1,9 @@
 function solution(n_str) {
-    for (let i = 0; i < n_str.length; i++) {
-        if (n_str[i] !== "0") {
-            return n_str.slice(i); 
-        }
-    }
-    return ""; 
+    // for (let i = 0; i < n_str.length; i++) {
+    //     if (n_str[i] !== "0") {
+    //         return n_str.slice(i); 
+    //     }
+    // }
+    // return ""; 
+    return String(Number(n_str));
 }
