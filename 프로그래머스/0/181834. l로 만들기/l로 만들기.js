fa4f1d8b70e1b50617d8fba3588e7877a7c60777
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-12-24 10:47:24 +0900

    [level 0] Title: l로 만들기, Time: 9.86 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,6 +1,7 @@
 function solution(myString) {
-    let arr = ['a','b','c','d','e','f','g','h','i','j','k'];
-    let strArr = myString.split('');
+//     let arr = ['a','b','c','d','e','f','g','h','i','j','k'];
+//     let strArr = myString.split('');
     
-    return strArr.map(el => arr.includes(el) ? "l" : el).join('');    
+//     return strArr.map(el => arr.includes(el) ? "l" : el).join('');    
+    return myString.split('').map(el => el < 'l' ? 'l' : el).join('');
 }
