99a0e6c62c0b05ea604a6e3737941193c892f6b1
jiahlee <129850495+JIAHLEE0213@users.noreply.github.com>
2024-12-31 14:30:55 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+   let result = [];
+    
+    while(n !== 1) {
+        if(n % 2 === 0) {
+            result.push(n);
+            n = n / 2;
+        } else {
+            result.push(n);
+            n = 3 * n + 1;
+        }
+    } 
+    result.push(1);
+    return result;
+}
